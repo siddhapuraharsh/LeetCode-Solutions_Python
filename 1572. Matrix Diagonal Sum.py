6c75aa8d01182151cac831445cd47c053c6d3da6
Harsh Siddhapura <82281356+siddhapuraharsh@users.noreply.github.com>
2023-02-20 02:53:50 +0530

    Easy

---
+++ 1572. Matrix Diagonal Sum.py
@@ -0,0 +1,18 @@
+#Link: https://leetcode.com/problems/matrix-diagonal-sum/
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+
+        n = len(mat)
+        primaryDiagonalSum = 0
+        secondaryDiagonalSum = 0
+
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    primaryDiagonalSum += mat[i][j]
+                elif (i+j) == (n-1):
+                    secondaryDiagonalSum += mat[i][j]
+
+        return primaryDiagonalSum + secondaryDiagonalSum
+			
